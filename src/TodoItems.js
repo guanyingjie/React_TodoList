8b6e27accd5b6ce720aa926183903720ae788e1a
yingjie.guan <yingjie.guan@yingjieguandeMac-mini.local>
2021-04-02 20:00:07 +0800

    add css

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class TodoItems extends React.Component{
     createTasks(Item){
-        return <li key{Item.key}>{Item.text}</li>
+        return <li key={Item.key}>{Item.text}</li>
     }
     render(){
         var todoentries = this.props.entries;
