e9e071ffee347b8d02b60e34abb9ba3ba01cbee2
yingjie.guan <yingjie.guan@yingjieguandeMac-mini.local>
2021-04-13 12:46:06 +0800

    add null

---
+++ showipaddress/src/ShowIpAddress.js
@@ -0,0 +1,19 @@
+import React,{Component} from "react";
+
+var xhr;
+
+class ShowIpAddress extends Component{
+
+
+    
+    
+
+    render(){
+        return (
+            <div>
+                nothing
+            </div>
+        );
+    }
+}
+export default ShowIpAddress;
